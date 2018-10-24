19772a0228c4518e688391ba2f29ad76cc954025
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-24 14:27:16 -0400

    This is my first commit for a project that demonstrates Inheritance and Polymorphism

---
+++ DevBuild.NerdsAndNumbers/GameCharacter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.NerdsAndNumbers {
+    public class GameCharacter {
+        public string   Name { get; set; }
+        public int      HitPoints { get; set; } = 10;
+        public int      Strength { get; set; }
+        public int      Intelligence { get; set; }
+
+        public GameCharacter(string characterName, int startingStrength, int startingIntelligence) {
+            Name = characterName;
+            Strength = startingStrength;
+            Intelligence = startingIntelligence;
+        }
+
+        public virtual void Play() {
+            Console.WriteLine($"{this.GetType().Name.ToString()}'s Name is {Name}");
+            Console.WriteLine($"{this.GetType().Name.ToString()}'s Strength is {Strength}");
+            Console.WriteLine($"{this.GetType().Name.ToString()}'s Intelligence is {Intelligence}");
+        }
+    }
+
+    public class Warrior : GameCharacter {
+
+        public string WeaponType { get; set; }
+
+        public Warrior(string characterName, int startingStrength, int startingIntelligence, string weapon) : 
+                        base(characterName, startingStrength, startingIntelligence) {
+            WeaponType = weapon;
+        }
+
+        public override void Play() {
+            base.Play();
+            Console.WriteLine($"{this.GetType().Name.ToString()}'s Weapon Type is {WeaponType}");
+        }
+    }
+
+    public class MagicUsingCharacter : GameCharacter {
+        public int MagicalEnergy { get; set; }
+
+        public MagicUsingCharacter( string characterName, int startingStrength, int startingIntelligence, int startingMagicalEnergy) : 
+                                        base(characterName, startingStrength, startingIntelligence) {
+            MagicalEnergy = startingMagicalEnergy;
+        }
+
+        public override void Play() {
+            base.Play();
+            Console.WriteLine($"{this.GetType().Name.ToString()}'s Magical Energy is {MagicalEnergy}");
+        }
+    }
+
+    class Wizard : MagicUsingCharacter {
+        public int SpellNumber { get; set; }
+        public Wizard(string characterName, int startingStrength, int startingIntelligence, int startingMagicalEnergy, int startingSpellNumber) : 
+                        base(characterName, startingStrength, startingIntelligence, startingMagicalEnergy) {
+            SpellNumber = startingSpellNumber;
+        }
+
+        public override void Play() {
+            base.Play();
+            Console.WriteLine($"{this.GetType().Name.ToString()}'s Spell Number is {SpellNumber}");
+        }
+    }
+}
--- DevBuild.NerdsAndNumbers/Program.cs
+++ DevBuild.NerdsAndNumbers/Program.cs
@@ -7,6 +7,20 @@ using System.Threading.Tasks;
 namespace DevBuild.NerdsAndNumbers {
     class Program {
         static void Main(string[] args) {
+            List<GameCharacter> gameCharacters = new List<GameCharacter>();
+
+            gameCharacters.Add(new Warrior("Brom", 150, 8, "a greatsword"));
+            gameCharacters.Add(new Warrior("Dirk", 999, 5, "a big-ass warhammer"));
+            gameCharacters.Add(new Wizard("Celeste", 6, 14, 150, 9));
+            gameCharacters.Add(new Wizard("Asher", 8, 12, 120, 15));
+            gameCharacters.Add(new Wizard("Morphumax", 5, 10, 80, 18));
+
+            foreach (GameCharacter c in gameCharacters) {
+                c.Play();
+                Console.WriteLine();
+            }
+
+            while (true) { }
         }
     }
 }
